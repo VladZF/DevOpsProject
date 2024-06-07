c9defc5f0b6b60bb1925d3aa5c7011dc541ebf8e
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-06-07 22:52:50 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,33 @@
+class Calculator:
+    @staticmethod
+    def sum(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def div(a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return 'Not defined'
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def int_div(a, b):
+        try:
+            return a // b
+        except ZeroDivisionError:
+            return 'Not defined'
+
+    @staticmethod
+    def rem(a, b):
+        try:
+            return a % b
+        except ZeroDivisionError:
+            return 'Not defined'
--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ from calculator import Calculator
     (-3, -7, -10),
     (9, 0, 9)
 ])
-def sum_test(a, b, result):
+def test_sum(a, b, result):
     assert Calculator.sum(a, b) == result
 
 
@@ -18,5 +18,48 @@ def sum_test(a, b, result):
     (-3, -7, 4),
     (9, 0, 9)
 ])
-def sub_test(a, b, result):
+def test_sub(a, b, result):
     assert Calculator.sub(a, b) == result
+
+
+@pytest.mark.parametrize('a, b, result', [
+    (1, 2, 2),
+    (2, -3, -6),
+    (-3, -7, 21),
+    (9, 0, 0)
+])
+def test_mul(a, b, result):
+    assert Calculator.mul(a, b) == result
+
+
+@pytest.mark.parametrize('a, b, result', [
+    (1, 2, 0.5),
+    (2, 1, 2),
+    (7, 0, 'Not defined'),
+    (0, 4, 0)
+])
+def test_div(a, b, result):
+    assert Calculator.div(a, b) == result
+
+
+@pytest.mark.parametrize('a, b, result', [
+    (1, 2, 0),
+    (5, 2, 2),
+    (7, 0, 'Not defined'),
+    (0, 4, 0),
+    (6, 3, 2),
+    (11, 6, 1)
+])
+def test_int_div(a, b, result):
+    assert Calculator.int_div(a, b) == result
+
+
+@pytest.mark.parametrize('a, b, result', [
+    (1, 2, 1),
+    (15, 5, 0),
+    (7, 0, 'Not defined'),
+    (13, 10, 3),
+    (21, 5, 1)
+])
+def test_rem(a, b, result):
+    assert Calculator.rem(a, b) == result
